0e9ceeb642e04c8384c017cc29f9d511b07aeff8
Sharmanitesh01 <ns4184727@gmail.com>
2026-08-15 17:22:05 +0530

    first-concepts

---
+++ first_folder/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div id="parent">
+      <h2 id="Child1">Hello</h2>
+      <h3 id="Child2">Hello 2</h3>
+    </div>
+  )
+}
+
+export default App
